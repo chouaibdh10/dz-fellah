1df551c6e73322b22c48e88699ab1763aa0e3026
chouaibdh10 <boudouhahmedchouaib@gmail.com>
2025-12-01 15:22:25 +0100

    ...

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -40,7 +40,7 @@ const Footer = () => {
       </div>
 
       <div className="footer-bottom">
-        <p>&copy; 2024 DZ-Fellah. Tous droits réservés.</p>
+        <p>&copy; 2025 DZ-Fellah. Tous droits réservés.</p>
       </div>
     </footer>
   )
